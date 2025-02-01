c2122021a60ea1738d45a3b836261b9eb5368565
minhnva <fe.minhnva@gmail.com>
2025-02-01 14:50:03 +0700

    update db connection

---
+++ src/config/databases.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+
+const mysql = require("mysql2");
+//Test connection
+
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password:process.env.DB_PASSWORD,
+  database:process.env.DB_NAME,
+});
+
+// const connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 3307,
+//   user: "root",
+//   password: "123456",
+//   database: "minhnva",
+// });
+
+module.exports = { connection };
--- src/config/viewEngine.js
+++ src/config/viewEngine.js
@@ -1,14 +1,13 @@
 const express = require("express");
 const path = require("path");
 
-
 const confgigViewEngine = (app) => {
   //config template engine
-  app.set("views", path.join('./src', "views"));
+  app.set("views", path.join("./src", "views"));
   app.set("view engine", "ejs");
 
   //config static folder
-  app.use(express.static(path.join('./src', "public")));
+  app.use(express.static(path.join("./src", "public")));
 };
 
 module.exports = confgigViewEngine;
